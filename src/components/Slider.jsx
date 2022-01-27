989596b7c64568ff3f4b633664cab019f0859eb8
Animesh Samaiya <asamaiya00@gmail.com>
2022-01-27 23:54:53 +0530

    feat: intersection observer effect

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,13 +1,18 @@
 import React from "react";
-
+import { useInView } from "react-intersection-observer";
 const Slider = ({ imageSrc, title, subtitle, flipped }) => {
+  const { ref, inView } = useInView({ threshold: 0.5 });
   return (
-    <div className={`slider ${flipped && "reverse"}`}>
+    <div
+      ref={ref}
+      className={`slider ${flipped && "reverse"} ${inView && "slider-zoom"}`}
+    >
       <img
         width={"200px"}
         height={"500px"}
         src={imageSrc}
         className="slider-image"
+        alt="StackOS"
       />
       <div className="slider-content">
         <h1>{title}</h1>
